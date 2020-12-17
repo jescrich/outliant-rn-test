c7a41a29479c7318a2190077acc41e94ac9bb2a5
Jose Escrich <jescrich@gmail.com>
2020-12-17 15:51:57 -0300

    Fix babel on ios

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['.'],
-        extensions: [
-          '.tsx',
-          '.ts',
-          '.jsx',
-          '.js',
-          '.json',
-        ],
-      },
-    ],
-  ],
 };
